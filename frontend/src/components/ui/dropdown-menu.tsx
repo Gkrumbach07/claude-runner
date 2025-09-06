292346ea4a092f16237f32778c7a64900b66175f
Gage Krumbach <gkrumbach@gmail.com>
2025-09-06 10:36:03 -0500

    Refactor dropdown menu icons in UI component
    - Replaced DotFilledIcon with Dot in DropdownMenuRadioItem for consistency in icon usage.
    - Updated import statement to reflect the change in icon component.

--- frontend/src/components/ui/dropdown-menu.tsx
+++ frontend/src/components/ui/dropdown-menu.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
-import { CheckIcon, ChevronRightIcon, DotFilledIcon } from "lucide-react";
+import { CheckIcon, ChevronRightIcon, Dot } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
@@ -130,7 +130,7 @@ const DropdownMenuRadioItem = React.forwardRef<
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <DropdownMenuPrimitive.ItemIndicator>
-        <DotFilledIcon className="h-4 w-4 fill-current" />
+        <Dot className="h-4 w-4 fill-current" />
       </DropdownMenuPrimitive.ItemIndicator>
     </span>
     {children}
